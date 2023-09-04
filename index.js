64c675700764c45c173796b28088f3df699efd44
honorific <honorific.info@gmail.com>
2023-09-04 21:25:32 +0330

    setup mongodb and connection

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import helmet from 'helmet'
+import morgan from 'morgan'
 
 dotenv.config()
 
 const app = express()
 
 const port = process.env.PORT
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(() => console.log("Couldn't connect to MongoDB"))
 
 app.listen(port, () => {
   console.log(`Backend running in port ${port}`)
