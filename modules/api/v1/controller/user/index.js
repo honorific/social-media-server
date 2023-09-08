b4fbacb5b5221ebf2ed2372b50b15f47198e9c17
honorific <honorific.info@gmail.com>
2023-09-08 11:49:20 +0330

    uodate user controller

--- modules/api/v1/controller/user/index.js
+++ modules/api/v1/controller/user/index.js
@@ -1,3 +1,25 @@
-export const updateUser = async (req,res) => {
-    
+import bcrypt from 'bcrypt'
+import User from '../../models/User'
+
+export const updateUser = async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10)
+        req.body.password = await bcrypt.hash(req.body.password, salt)
+      } catch (error) {
+        return res.status(500).json(error)
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      })
+      return res.status(200).json('account has been updated')
+    } catch (error) {
+      return res.status(500).json(error)
+    }
+  } else {
+    return res.status(403).json('You can update only your account!')
+  }
 }
