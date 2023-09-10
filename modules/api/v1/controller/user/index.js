368a5bce409f93b4c0d7e491367a42d142a76f78
honorific <honorific.info@gmail.com>
2023-09-10 19:21:01 +0330

    delete controller

--- modules/api/v1/controller/user/index.js
+++ modules/api/v1/controller/user/index.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt'
 import User from '../../models/User.js'
 
 export const updateUser = async (req, res) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin) {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
       try {
         const salt = await bcrypt.genSalt(10)
@@ -25,24 +25,14 @@ export const updateUser = async (req, res) => {
 }
 
 export const deleteUser = async (req, res) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin) {
-    if (req.body.password) {
-      try {
-        const salt = await bcrypt.genSalt(10)
-        req.body.password = await bcrypt.hash(req.body.password, salt)
-      } catch (error) {
-        return res.status(500).json(error)
-      }
-    }
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     try {
-      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-      })
-      return res.status(200).json('account has been updated')
+      const user = await User.findByIdAndDelete(req.params.id)
+      return res.status(200).json('account has been deleted')
     } catch (error) {
       return res.status(500).json(error)
     }
   } else {
-    return res.status(403).json('You can update only your account!')
+    return res.status(403).json('You can delete only your account!')
   }
 }
