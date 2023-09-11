f73ec16e97151b58b97625badaeaf7faf0478e85
honorific <honorific.info@gmail.com>
2023-09-11 12:16:06 +0330

    get user contoller

--- modules/api/v1/controller/user/index.js
+++ modules/api/v1/controller/user/index.js
@@ -36,3 +36,12 @@ export const deleteUser = async (req, res) => {
     return res.status(403).json('You can delete only your account!')
   }
 }
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json({error})
+  }
+}
