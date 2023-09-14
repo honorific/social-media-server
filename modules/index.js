1fdf4e2030da4be5dba7572ba190eddd9fc1450e
honorific <honorific.info@gmail.com>
2023-09-14 15:56:59 +0330

    configuring posts route

--- modules/index.js
+++ modules/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import userRouter from './api/v1/routes/users.js'
 import authRouter from './api/v1/routes/auth.js'
+import postRouter from './api/v1/routes/posts.js'
 const router = express.Router()
 
 router.use('/api/v1/users', userRouter)
 router.use('/api/v1/auth', authRouter)
-
+router.use('/api/v1/posts', postRouter)
 export default router
