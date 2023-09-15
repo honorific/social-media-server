3960af2cbe2d6ee7514f5b9539cca2b065eee14c
honorific <honorific.info@gmail.com>
2023-09-15 11:53:13 +0330

    update post controller

--- modules/api/v1/controller/posts/index.js
+++ modules/api/v1/controller/posts/index.js
@@ -9,3 +9,17 @@ export const newPost = async (req, res) => {
     res.status(500).json(error)
   }
 }
+
+export const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (post.userId == req.body.userId) {
+      await post.updateOne({$set: req.body})
+      res.status(200).json('post updated')
+    } else {
+      res.status(403).json('you can update only your post')
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
