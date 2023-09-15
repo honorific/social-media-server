f50612a6afd26db2a49ac72f684fe2576a61a81d
honorific <honorific.info@gmail.com>
2023-09-15 11:59:07 +0330

    delete post route

--- modules/api/v1/routes/posts.js
+++ modules/api/v1/routes/posts.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import {newPost, updatePost} from '../controller/posts/index.js'
+import {deletePost, newPost, updatePost} from '../controller/posts/index.js'
 const router = express.Router()
 
 router.post('/', newPost)
 router.put('/:id', updatePost)
+router.delete('/:id', deletePost)
 
 export default router
