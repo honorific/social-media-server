1cf3e024ee2251eda5ad1a20cb46b0e461fa23a0
honorific <honorific.info@gmail.com>
2023-09-16 12:07:03 +0330

    getPost router

--- modules/api/v1/controller/posts/index.js
+++ modules/api/v1/controller/posts/index.js
@@ -37,3 +37,18 @@ export const deletePost = async (req, res) => {
     res.status(500).json(error)
   }
 }
+
+export const likePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (!post.likes.includes(req.body.userId)) {
+      await Post.updateOne({$push: {likes: req.body.userId}})
+      res.status(200).json('post has been liked')
+    } else {
+      await Post.updateOne({$pull: {likes: req.body.userId}})
+      res.status(200).json('post has been disliked')
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
--- modules/api/v1/routes/posts.js
+++ modules/api/v1/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import {deletePost, newPost, updatePost} from '../controller/posts/index.js'
+import {deletePost, likePost, newPost, updatePost} from '../controller/posts/index.js'
 const router = express.Router()
 
 router.post('/', newPost)
 router.put('/:id', updatePost)
 router.delete('/:id', deletePost)
-router.put('/:id/like')
+router.put('/:id/like', likePost)
+router.get('/:id', getPost)
 
 export default router
