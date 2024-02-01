f485126e97946abb402e23198d85431b70304a46
honorific <honorific.info@gmail.com>
2024-02-01 20:14:09 +0330

    the true getUserPosts controller

--- modules/api/v1/controller/posts/index.js
+++ modules/api/v1/controller/posts/index.js
@@ -81,15 +81,9 @@ export const getTimeline = async (req, res) => {
 
 export const getUserPosts = async (req, res) => {
   try {
-    const currentUser = await User.findById(req.params.userId)
-    const userPosts = await Post.find({userId: currentUser._id})
-    const friendsPosts = await Promise.all(
-      currentUser.followings.map((friendId) => {
-        return Post.find({userId: friendId})
-      }),
-    )
-    console.log('friendPosts is: ', friendsPosts)
-    res.status(200).json(userPosts.concat(...friendsPosts))
+    const user = await User.findOne({username: req.params.username})
+    const posts = await Post.find({userId: user._id})
+    res.status(200).json(posts)
   } catch (error) {
     res.status(500).json(error)
   }
--- modules/api/v1/routes/posts.js
+++ modules/api/v1/routes/posts.js
@@ -16,6 +16,6 @@ router.delete('/:id', deletePost)
 router.put('/:id/like', likePost)
 router.get('/:id', getPost)
 router.get('/timeline/:userId', getTimeline)
-route.get('/profile/:username', getUserPosts)
+router.get('/profile/:username', getUserPosts)
 
 export default router
