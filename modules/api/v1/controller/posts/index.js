acfb62d1230c23de9dc71aaae8c932f55c667731
honorific <honorific.info@gmail.com>
2024-02-16 18:27:34 +0330

    fix liking diffrent post

--- modules/api/v1/controller/posts/index.js
+++ modules/api/v1/controller/posts/index.js
@@ -43,10 +43,10 @@ export const likePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id)
     if (!post.likes.includes(req.body.userId)) {
-      await Post.updateOne({$push: {likes: req.body.userId}})
+      await post.updateOne({$push: {likes: req.body.userId}})
       res.status(200).json('post has been liked')
     } else {
-      await Post.updateOne({$pull: {likes: req.body.userId}})
+      await post.updateOne({$pull: {likes: req.body.userId}})
       res.status(200).json('post has been disliked')
     }
   } catch (error) {
