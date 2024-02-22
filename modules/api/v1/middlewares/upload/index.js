3fa214f21c89fea72a905c2c666117812acf611b
honorific <honorific.info@gmail.com>
2024-02-22 19:04:43 +0330

    multer config for upload post image

--- modules/api/v1/middlewares/upload/index.js
+++ modules/api/v1/middlewares/upload/index.js
@@ -5,6 +5,7 @@ const storage = multer.diskStorage({
     cb(null, 'public/images')
   },
   filename: (req, file, cb) => {
+    //req.body.name works because it defined before file data in front-end share component
     cb(null, req.body.name)
   },
 })
