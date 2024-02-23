be925e54f8d5dd411ea9d57270c676829126bfee
honorific <honorific.info@gmail.com>
2024-02-23 20:38:27 +0330

    get friends route and controller

--- modules/api/v1/controller/user/index.js
+++ modules/api/v1/controller/user/index.js
@@ -37,6 +37,25 @@ export const deleteUser = async (req, res) => {
   }
 }
 
+export const getFriends = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId)
+    const friends = await Promise.all(
+      user.followings.map((friendId) => {
+        return User.findById(friendId)
+      }),
+    )
+    let friendList = []
+    friends.map((friend) => {
+      const {_id, username, profilePicture} = friend
+      friendList.push({_id, username, profilePicture})
+    })
+    res.status(200).json(friendList)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 export const getUser = async (req, res) => {
   const userId = req.query.userId
   const username = req.query.username
