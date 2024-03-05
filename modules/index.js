4f01523530a0195fafc352046afff5bb7c1874be
honorific <honorific.info@gmail.com>
2024-03-05 17:25:07 +0330

    new conversation route and controller

--- modules/index.js
+++ modules/index.js
@@ -3,11 +3,13 @@ import userRouter from './api/v1/routes/users.js'
 import authRouter from './api/v1/routes/auth.js'
 import postRouter from './api/v1/routes/posts.js'
 import uploadRouter from './api/v1/routes/upload.js'
+import conversationRouter from './api/v1/routes/conversations.js'
 const router = express.Router()
 
 router.use('/api/v1/users', userRouter)
 router.use('/api/v1/auth', authRouter)
 router.use('/api/v1/posts', postRouter)
 router.use('/api/v1/upload', uploadRouter)
+router.use('/api/v1/conversations', conversationRouter)
 
 export default router
