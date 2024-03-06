2970ae4dc55abb098ae25a792e5ab990ff02516b
honorific <honorific.info@gmail.com>
2024-03-06 17:54:45 +0330

    getConversation route and controller

--- modules/api/v1/routes/conversations.js
+++ modules/api/v1/routes/conversations.js
@@ -1,7 +1,11 @@
 import express from 'express'
-import {newConversation} from '../controller/conversations/index.js'
+import {
+  newConversation,
+  getConversation,
+} from '../controller/conversations/index.js'
 const router = express.Router()
 
 router.post('/', newConversation)
+router.get('/:userId', getConversation)
 
 export default router
