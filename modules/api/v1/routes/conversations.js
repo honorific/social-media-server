effe9880293ecc3e02a752ebcc8bd0f1ae4392ca
honorific <honorific.info@gmail.com>
2024-04-04 21:08:22 +0330

    getConversationWithTwoUserId controller

--- modules/api/v1/routes/conversations.js
+++ modules/api/v1/routes/conversations.js
@@ -2,10 +2,12 @@ import express from 'express'
 import {
   newConversation,
   getConversation,
+  getConversationWithTwoUserId,
 } from '../controller/conversations/index.js'
 const router = express.Router()
 
 router.post('/', newConversation)
 router.get('/:userId', getConversation)
+router.get('/find/:firstUserId/:secondUserId', getConversationWithTwoUserId)
 
 export default router
